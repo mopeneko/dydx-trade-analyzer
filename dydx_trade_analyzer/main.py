f2add6a417e852702b8e78a740e52b13db15a570
mopeneko <mopeneko@lem0n.cc>
2023-12-30 05:28:36 +0900

    Implement main feature

---
+++ dydx_trade_analyzer/main.py
@@ -0,0 +1,64 @@
+from datetime import datetime, time
+import pytz
+import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+def get_timestamp(d, tz: str, end=False) -> int:
+    native_dt = datetime.combine(d, time())
+    if end:
+        native_dt = native_dt.replace(hour=23, minute=59, second=59)
+    dt = pytz.timezone(tz).localize(native_dt)
+    return int(dt.timestamp() * 1000)
+
+
+st.set_page_config(page_title="dYdX Trade Analyzer")
+st.title("dYdX Trade Analyzer")
+
+uploaded_file = st.file_uploader("Upload a CSV file")
+
+today = datetime.today()
+begging_of_month = datetime(today.year, today.month, 1)
+d = st.date_input(
+    "Select duration",
+    (begging_of_month, today),
+    begging_of_month,
+    today,
+)
+
+timezone = st.selectbox(
+    "Select timezone",
+    ("UTC", "JST"),
+)
+
+if uploaded_file is not None and len(d) == 2:
+    df = pd.read_csv(uploaded_file)
+
+    tz = "UTC"
+    match timezone:
+        case "JST":
+            tz = "Asia/Tokyo"
+
+    start_timestamp = get_timestamp(d[0], tz)
+    end_timestamp = get_timestamp(d[1], tz, end=True)
+
+    df = df.where(df["Opened"] >= start_timestamp)
+    df = df.where(df["Opened"] <= end_timestamp)
+
+    df["Leveraged PnL"] = df["PnL"] * df["Leverage"]
+    st.write("PnL(sum):", df['Leveraged PnL'].sum(), "USD")
+    st.write("PnL(mean):", df['Leveraged PnL'].mean(), "USD")
+
+    df_long = df.where(df["Type"] == "Long")
+    df_short = df.where(df["Type"] == "Short")
+
+    st.write("Long PnL(mean):", df_long['Leveraged PnL'].mean(), "USD")
+    st.write("Short PnL(mean):", df_short['Leveraged PnL'].mean(), "USD")
+
+    plot = sns.catplot(x="Type", y="Leveraged PnL", data=df, kind="box")
+    st.pyplot(plot.fig)
+
+    plot = sns.catplot(x="Market", y="Leveraged PnL", data=df, kind="box", hue="Type")
+    st.pyplot(plot.fig)
